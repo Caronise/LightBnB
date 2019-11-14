05b8826bcfbd0b499249d11992bee06ef1c4b0c3
Caronise <lucas.betancourt@hotmail.com>
2019-11-14 21:12:49 +0000

    Installed dependencies, connected using pool and modified getUserWithEmail and getUserWithId to use the database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,6 +1,21 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+pool.connect()
+  .then(() => {
+    console.log("Succesfully connected to database!");
+  }).catch((error) => {
+    console.log(`Error is: ${error}`)
+});
+
 /// Users
 
 /**
@@ -9,16 +24,21 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  const text = `SELECT *
+                FROM users
+                WHERE email = $1`;
+  const values = [email];
+  
+  return pool.query(text, values)
+    .then((res) => {
+      if (res.rows.length === 0) {
+        return(null)
+      } else {
+        return(res.rows[0])
+      }
+    }).catch((error) => {
+      console.log(`Error is: ${error}`)
+    });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -28,7 +48,23 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  const text = `
+  SELECT *
+  FROM users
+  WHERE id = $1`;
+  const values = [id];
+
+  return pool.query(text, values)
+    .then((res) => {
+      if (res.rows.length === 0) {
+        return (null)
+      } else {
+        return (res.rows[0])
+      }
+    }).catch((error) => {
+      console.log(`Error is: ${error}`)
+  });
+
 }
 exports.getUserWithId = getUserWithId;
 
@@ -67,11 +103,13 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  
+  return pool.query(`
+  SELECT * FROM properties
+  LIMIT $1
+  `, [limit])
+  .then(res => res.rows);
+  
 }
 exports.getAllProperties = getAllProperties;
 
